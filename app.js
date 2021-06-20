2ad7a995b0e5cb0015d494bd7e8675ff58ecc8ba
Ndibechinonso <edwardndibe@gmail.com>
2021-06-20 12:51:17 +0100

    readme

--- app.js
+++ app.js
@@ -30,11 +30,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors())
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use('/', indexRouter);
 // app.use(session({secret: 'ssshhhhh', resave: false, saveUninitialized: true}));
